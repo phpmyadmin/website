f747a663a4ad1be5b4a223379f2e697bf94987a3
Michal Čihař <michal@cihar.com>
2008-11-21 08:46:57 +0000

    Missing dot...

--- static/_security.php
+++ static/_security.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($$_GET['issue'])) {
-    header("Location: ${server}${base_url}security/" . $$_GET['issue'] . "${file_ext}");
+    header("Location: ${server}${base_url}security/" . $$_GET['issue'] . ".${file_ext}");
 } else {
     header("Location: ${server}${base_url}security/");
 }
