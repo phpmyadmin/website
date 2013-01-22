fd0b157239f5e21953e7f6a99c2b828d99729f95
Marc Delisle <marc@infomarc.info>
2013-01-21 20:05:17 -0500

    Detect new major version

--- render.py
+++ render.py
@@ -231,6 +231,8 @@ class SFGenerator:
             text ='Version compatible with PHP 4+ and MySQL 3+.'
         elif version[:2] == '3.':
             text = 'Version compatible with PHP 5.2 and MySQL 5.'
+        elif version[:2] == '4.':
+            text = 'Version compatible with PHP 5.2 and MySQL 5. Requires a Javascript-enabled browser.'
         if version.find('beta1') != -1:
             text += ' First beta version.'
         elif version.find('beta2') != -1:
