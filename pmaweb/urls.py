9b1656b30143d97e768f7c5d528a3261b7b6c112
Michal Čihař <michal@cihar.com>
2015-06-22 14:50:04 +0200

    Add swekey redirect
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- pmaweb/urls.py
+++ pmaweb/urls.py
@@ -1,10 +1,18 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
+
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'pmaweb.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(
+        r'auth_key',
+        RedirectView.as_view(
+            url='http://store.swekey.com/index.php?promo=pma'
+        )
+    ),
     url(r'^admin/', include(admin.site.urls)),
 )
