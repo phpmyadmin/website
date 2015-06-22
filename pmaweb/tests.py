4b4cff4b7482c669184e219ee587a372fdd6a4e8
Michal Čihař <michal@cihar.com>
2015-06-22 20:14:46 +0200

    Add view tests with fixtures
    Signed-off-by: Michal Čihař <michal@cihar.com>

---
+++ pmaweb/tests.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+
+
+class ViewTest(TestCase):
+    fixtures = ['test_data.json']
+
+    def test_home(self):
+        response = self.client.get(reverse('home'))
+        self.assertContains(response, 'phpMyAdmin')
+        response = self.client.get('/')
+        self.assertContains(response, 'phpMyAdmin')
