d5b8fc4dd44824dcfe5297cfcd7732c4297028ab
Michal Čihař <michal@cihar.com>
2015-06-26 13:25:15 +0200

    Revert "Debug travis failure"
    This reverts commit 70ff00a2bbf6ce6c233896e4fd64c88e4774bdb1.

--- pmaweb/tests.py
+++ pmaweb/tests.py
@@ -50,7 +50,6 @@ class ViewTest(TestCase):
 
     def test_redirects(self):
         for url in REDIRECT_MAP:
-            print '/home_page/{0}.php'.format(url)
             response = self.client.get(
                 '/home_page/{0}.php'.format(url),
                 follow=True
