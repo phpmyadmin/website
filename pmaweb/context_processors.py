f905d5bdaef6c59a97dc50ef444608ae438682fd
Michal Čihař <michal@cihar.com>
2015-06-26 11:22:27 +0200

    Handle situation with no beta release
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- pmaweb/context_processors.py
+++ pmaweb/context_processors.py
@@ -75,8 +75,11 @@ def menu(request):
 def releases(request=None):
 
     latest = Release.objects.filter(stable=True)[0]
-    beta = Release.objects.filter(stable=False)[0]
-    if beta.version_num < latest.version_num:
+    if Release.objects.filter(stable=False).exist():
+        beta = Release.objects.filter(stable=False)[0]
+        if beta.version_num < latest.version_num:
+            beta = None
+    else:
         beta = None
 
     delta = 1000000
