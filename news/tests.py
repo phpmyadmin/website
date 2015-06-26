d1261c00023b5495151f0a05f756615eddc1820d
Michal Čihař <michal@cihar.com>
2015-06-26 10:34:18 +0200

    Remove unused import
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- news/tests.py
+++ news/tests.py
@@ -19,6 +19,5 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
-from django.test import TestCase
 
 # Create your tests here.
