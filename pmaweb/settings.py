360b6e8a35d3375fca456492b01d07eaf8bdd33e
Michal Čihař <michal@cihar.com>
2015-06-29 09:08:06 +0200

    Include sites app
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- pmaweb/settings.py
+++ pmaweb/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'django.contrib.sitemaps',
     'files',
     'news',
