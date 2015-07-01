9577d5acbabb4b82e46c1449367f02364a10c5d6
Marc Delisle <marc@infomarc.info>
2015-07-01 10:07:27 -0400

    End of support for 4.2.x
    Signed-off-by: Marc Delisle <marc@infomarc.info>

--- pmaweb/settings.py
+++ pmaweb/settings.py
@@ -139,7 +139,7 @@ FILES_PATH = os.path.join(BASE_DIR, 'frs')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # List of major versions to offer for download
-LISTED_BRANCHES = ('4.4', '4.3', '4.2', '4.0')
+LISTED_BRANCHES = ('4.4', '4.3', '4.0')
 
 CDN_LOGIN = 'admins@phpmyadmin.net'
 CDN_PASSWORD = ''
