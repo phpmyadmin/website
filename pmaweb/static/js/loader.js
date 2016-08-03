437e90fc02cd297519cd35e0bfb876e19ee279ef
Michal Čihař <michal@cihar.com>
2016-08-03 17:51:26 +0200

    Add image rotation script
    Signed-off-by: Michal Čihař <michal@cihar.com>

---
+++ pmaweb/static/js/loader.js
@@ -0,0 +1,14 @@
+function cycleImages(){
+      var $active = $('#cycler .active');
+      var $next = ($active.next().length > 0) ? $active.next() : $('#cycler img:first');
+      $next.css('z-index',2);//move the next image up the pile
+      $active.fadeOut(1500,function(){//fade out the top image
+	  $active.css('z-index',1).show().removeClass('active');//reset the z-index and unhide the image
+          $next.css('z-index',3).addClass('active');//make the next image the top one
+      });
+    }
+
+$(document).ready(function(){
+    // run every 5s
+    setInterval('cycleImages()', 5000);
+})
