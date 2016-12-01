c9c48679599cf3212a5cb3fdbce889a99dc8d6ec
Michal Čihař <michal@cihar.com>
2016-12-01 11:08:42 +0100

    No longer offer 4.4 for download
    It has reached EOL month ago.
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- pmaweb/settings.py
+++ pmaweb/settings.py
@@ -162,7 +162,7 @@ COMPRESS_CSS_FILTERS = [
 ]
 
 # List of major versions to offer for download
-LISTED_BRANCHES = ('4.6', '4.4', '4.0')
+LISTED_BRANCHES = ('4.6', '4.0')
 
 CDN_LOGIN = 'admins@phpmyadmin.net'
 CDN_PASSWORD = ''
