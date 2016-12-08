50f7416ce71bc2d5d72cd29d1bb9d378e032c29d
Michal Čihař <michal@cihar.com>
2016-12-08 08:43:40 +0100

    Use event tracking for downloads
    It seems better fit for tracking external links.
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- pmaweb/static/js/loader.js
+++ pmaweb/static/js/loader.js
@@ -67,7 +67,7 @@ $(function(){
     $('.download_popup').click(function () {
         var $this = $(this);
         var pgp = $this.data('pgp');
-        ga('send', 'pageview', $this.attr('href'));
+        ga('send', 'event', 'Download', 'click', $this.attr('href'));
         $('#dl-link').attr('href', $this.attr('href'));
         $('#dl-sha1').text($this.data('sha1'));
         $('#dl-sha256').text($this.data('sha256'));
