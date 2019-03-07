a8818673a377b3670c78f3fa96578d402664ee00
Michal Čihař <michal@cihar.com>
2019-03-07 14:41:03 +0100

    Remove 4.0 from listing on download page
    Fixes https://github.com/phpmyadmin/phpmyadmin/issues/15005
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- pmaweb/settings.py
+++ pmaweb/settings.py
@@ -162,7 +162,7 @@ COMPRESS_CSS_FILTERS = [
 ]
 
 # List of major versions to offer for download
-LISTED_BRANCHES = ('4.8', '4.0')
+LISTED_BRANCHES = ('4.8',)
 
 CDN_LOGIN = 'admins@phpmyadmin.net'
 CDN_PASSWORD = ''
