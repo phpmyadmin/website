b909bb1a07806c5b04f2175eb018d35ba3d70303
William Desportes <williamdes@wdes.fr>
2021-02-24 11:05:14 +0100

    Add 5.1 to branches
    Signed-off-by: William Desportes <williamdes@wdes.fr>

--- pmaweb/settings.py
+++ pmaweb/settings.py
@@ -162,7 +162,7 @@ COMPRESS_CSS_FILTERS = [
 ]
 
 # List of major versions to offer for download in descending order.
-LISTED_BRANCHES = ('5.0','4.9')
+LISTED_BRANCHES = ('5.1','5.0','4.9')
 
 CDN_LOGIN = 'admins@phpmyadmin.net'
 CDN_PASSWORD = ''
